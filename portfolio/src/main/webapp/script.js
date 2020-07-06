c7b5303a7abb01d4f8b4058c8d8c9cc82520a440
Bhargavi Sundaresan <bhargavi.kumar2012@gmail.com>
2020-07-06 19:44:46 +0000

    indents index.html, images.html, and photo.html, and script.js in 2 space indentation style.

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -1,28 +1,28 @@
-// Copyright 2019 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     https://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+function randomizeQuote() {
 
-/**
- * Adds a random greeting to the page.
- */
-function addRandomGreeting() {
-  const greetings =
-      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
+  var imageIndex = Math.floor(Math.random() * 4) + 1;
 
-  // Pick a random greeting.
-  const greeting = greetings[Math.floor(Math.random() * greetings.length)];
+  //To check if an image exists already
+  if($('#generated-quote-id').length>0)
+  {
+    //Image exists
+    var currentImageIndex = $('#generated-quote-id').attr('src').substr(13,1)
+    while(imageIndex+"" === currentImageIndex)
+    {
+      imageIndex = Math.floor(Math.random() * 4) + 1;
+    }
+    
+  }
 
-  // Add it to the page.
-  const greetingContainer = document.getElementById('greeting-container');
-  greetingContainer.innerText = greeting;
+  const imgUrl = '/quotes/quote' + imageIndex + '.jpg';
+  const imgElement = document.createElement('img');
+  imgElement.id = "generated-quote-id";
+  imgElement.src = imgUrl;
+  const imageContainer = document.getElementById('random-quote-container');
+  // Remove the previous image.
+  imageContainer.innerHTML = '';
+  imageContainer.appendChild(imgElement);
+  $('#generated-quote-id').width(1000);
+  $('#generated-quote-id').height(700);
+
 }
