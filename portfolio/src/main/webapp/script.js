ffa1e63550c7fe14db56b922d2e2370c3093bf0d
Bhargavi Sundaresan <bhargavi.kumar2012@gmail.com>
2020-07-07 00:36:32 +0000

    makes the randomizeQuote() more efficient, and declares the highest number as a constant.

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -2,20 +2,32 @@
 // This function generates a random quote to display, also making sure that the previously displayed random image is not the same as the current random image.
 function randomizeQuote() {
 
+  const range= 4;    // This constant variable keeps track of the number of quotes I have. 
+
   // ImageNumber has the Image index of the quotes. This generates a number between [1-4]
-  var imageNumber = Math.floor(Math.random() * 4) + 1;
+  var imageNumber = Math.floor(Math.random() * range) + 1;
 
   // To check if an image exists already. The length of the list returned must have some element in it. This means that this is not the first time the button was clicked after page reload. 
   if($('#generated-quote-id').length>0)
   {
     var displayedImageNumber = $('#generated-quote-id').attr('src').substr(13,1);    // This gets the number(index) of the image that was displayed right before the button was clicked. 
 
-    while(imageNumber+"" === displayedImageNumber){        // Checks if the newly generated index is the same as the previously displayed image. as long as it is the same, it generates a different index
-      imageNumber = Math.floor(Math.random() * 4) + 1;        
+    // If the displayed picture and the currently generated image have the same index, 
+    if(imageNumber+"" === displayedImageNumber)
+    {
+      // And if the Number is the last value in the set of images, simply make sure to print the first picture of the bunch.
+      if(imageNumber === range) {
+        imageNumber = 1;
+      }
+
+      // If it's not the highest number, then simply add 1 to the randomized index.
+      else {
+        imageNumber = imageNumber + 1;
+      } 
     }
     
-    // Here, the loop is broken and a different index has been stored in imageNumber than the previously generated quote.
   }
+  
 
   // Now, the page has to link the image and the division in quote.html
   const imgUrl = '/quotes/quote' + imageNumber + '.jpg';                         // Generates image url
