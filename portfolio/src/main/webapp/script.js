2df5a7c96711f4ff3ea016577251087c79287551
Bhargavi Sundaresan <bhargavi.kumar2012@gmail.com>
2020-07-07 00:05:23 +0000

    resolves Isaac's feedback and David's multiline commenting feedback

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -1,32 +1,32 @@
 
-//this function generates a random quote to display, also making sure that the previously displayed random image is not the same as the current random image.
+// This function generates a random quote to display, also making sure that the previously displayed random image is not the same as the current random image.
 function randomizeQuote() {
 
-  //ImageNumber has the Image index of the quotes. This generates a number between [1-4]
+  // ImageNumber has the Image index of the quotes. This generates a number between [1-4]
   var imageNumber = Math.floor(Math.random() * 4) + 1;
 
-  //To check if an image exists already. The length of the list returned must have some element in it. This means that this is not the first time the button was clicked after page reload. 
+  // To check if an image exists already. The length of the list returned must have some element in it. This means that this is not the first time the button was clicked after page reload. 
   if($('#generated-quote-id').length>0)
   {
-    var displayedImageIndex = $('#generated-quote-id').attr('src').substr(13,1);    //This gets the number(index) of the image that was displayed right before the button was clicked. 
+    var displayedImageNumber = $('#generated-quote-id').attr('src').substr(13,1);    // This gets the number(index) of the image that was displayed right before the button was clicked. 
 
-    while(imageNumber+"" === displayedImageIndex){        //checks if the newly generated index is the same as the previously displayed image. as long as it is the same, it generates a different index
+    while(imageNumber+"" === displayedImageNumber){        // Checks if the newly generated index is the same as the previously displayed image. as long as it is the same, it generates a different index
       imageNumber = Math.floor(Math.random() * 4) + 1;        
     }
     
-    //here, the loop is broken and a different index has been stored in imageNumber than the previously generated quote.
+    // Here, the loop is broken and a different index has been stored in imageNumber than the previously generated quote.
   }
 
-  //now, the page has to link the image and the division in quote.html
-  const imgUrl = '/quotes/quote' + imageNumber + '.jpg';                         //generates image url
-  const imgElement = document.createElement('img');                              //creates a stand alone image tag
-  imgElement.id = "generated-quote-id";                                          //sets its ID
-  imgElement.src = imgUrl;                                                       //assigned the above image url as the src of the image tag
-  const imageContainer = document.getElementById('random-quote-container');      //gets the contained into which the image has to be stored
+  // Now, the page has to link the image and the division in quote.html
+  const imgUrl = '/quotes/quote' + imageNumber + '.jpg';                         // Generates image url
+  const imgElement = document.createElement('img');                              // Creates a stand alone image tag
+  imgElement.id = "generated-quote-id";                                          // Sets its ID
+  imgElement.src = imgUrl;                                                       // Assigned the above image url as the src of the image tag
+  const imageContainer = document.getElementById('random-quote-container');      // Gets the contained into which the image has to be stored
   // Remove the previous image.
-  imageContainer.innerHTML = '';                                                 //clears up any image that already existed inside it
-  imageContainer.appendChild(imgElement);                                        //appends the image into the div as a direct child
-  $('#generated-quote-id').width(1000);                                          //specifies the width of the image
-  $('#generated-quote-id').height(650);                                          // specifies the height of the image
+  imageContainer.innerHTML = '';                                                 // Clears up any image that already existed inside it
+  imageContainer.appendChild(imgElement);                                        // Appends the image into the div as a direct child
+  $('#generated-quote-id').width(1000);                                          // Specifies the width of the image
+  $('#generated-quote-id').height(650);                                          // Specifies the height of the image
 
 }
