3b89a1cafeba5ca058534379173ed0d65ca5cc73
Bhargavi Sundaresan <bhargavi.kumar2012@gmail.com>
2020-07-10 22:30:38 +0000

    creates comments and stores them with datastore and entities

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -14,8 +14,16 @@
 
 package com.google.sps.servlets;
 
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.SortDirection;
+import com.google.sps.data.Comment;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import com.google.gson.Gson;
 import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
@@ -23,57 +31,30 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/** Servlet that returns some example content. TODO: modify this file to handle comments data */
+/** Servlet responsible for listing all the comments from datastore. */
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
-  
-  private ArrayList<String> messages;
 
-  /** Initializing the arraylist that holds the messages with hard-coded strings */
-  @Override
-  public void init() {
-    messages = new ArrayList<>();
-  }
-
-  /* This function is executed when fetch('/data') is called. The goal of this function is to create a gson object, conver messages into gson, and send it bac to script.js */
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    
-    Gson g = new Gson();                                // Imported in pom and above
-    String json = g.toJson(messages);
-    response.setContentType("application/json;");
-    response.getWriter().println(json);
-    /* 
-      TODO:
-      response.setContentType("text/html;");
-      response.getWriter().println("<h1>Hello Bhargavi!</h1>");   // Added my name instead of  World.
-      response.getWriter().println("<h2>I am learning Servlets!</h2>");
-    */
-  }
+    Query query = new Query("Comment").addSort("timestamp", SortDirection.DESCENDING);
 
-  @Override
-  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    // Get the input from the form.
-    String text = getParameter(request, "text-input", "");
-    messages.add(text);
-    response.sendRedirect("/comments.html");
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    PreparedQuery results = datastore.prepare(query);
 
-    // TODO: Respond with the result.
-    // response.setContentType("text/html;");
-    // response.getWriter().println(Arrays.toString(messages));
-  }
+    List<Comment> comments = new ArrayList<>();
+    for (Entity entity : results.asIterable()) {
+      long id = entity.getKey().getId();
+      String text = (String) entity.getProperty("comment");
+      long timestamp = (long) entity.getProperty("timestamp");
 
-
-  /**
-   * @return the request parameter, or the default value if the parameter
-   *         was not specified by the client
-   */
-  private String getParameter(HttpServletRequest request, String name, String defaultValue) {
-    String value = request.getParameter(name);
-    if (value == null) {
-      return defaultValue;
+      Comment comment = new Comment(id, text, timestamp);
+      comments.add(comment);
     }
-    return value;
-  }
 
+    Gson gson = new Gson();
+
+    response.setContentType("application/json;");
+    response.getWriter().println(gson.toJson(comments));
+  }
 }
